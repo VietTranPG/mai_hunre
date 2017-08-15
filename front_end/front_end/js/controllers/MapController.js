428c536c28ddcfde759716e4509f20734220b176
viettranpg <viet.tranhoang@powergatesoftware.com>
2017-08-15 17:29:19 +0700

    ÃZ

--- front_end/front_end/js/controllers/MapController.js
+++ front_end/front_end/js/controllers/MapController.js
@@ -1,5 +1,12 @@
 ﻿angular.module('MetronicApp').controller('MapController', ['$rootScope', '$scope', 'settings', function ($rootScope, $scope, settings) {
-    $scope.$on('$viewContentLoaded', function () {
-       
+    angular.element(document).ready(function () {
+        initAutocomplete();
     });
+    function initAutocomplete() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+            center: { lat: -33.8688, lng: 151.2195 },
+            zoom: 13,
+            mapTypeId: 'roadmap'
+        });
+    }
 }]);
