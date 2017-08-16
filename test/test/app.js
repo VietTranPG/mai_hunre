da2a6e1bd823a1d85e715369931c79e734f4cb1f
viettranpg <viet.tranhoang@powergatesoftware.com>
2017-08-16 17:39:32 +0700

    add angular

---
+++ test/test/app.js
@@ -0,0 +1,46 @@
+﻿var app = angular.module('myApp', []);
+app.controller('CardCtrl', function ($scope, $http, commonService) {
+    var map;
+    var iconParty = '../images/party.png';
+    var iconDanger = '../images/danger.png';
+    var listPolice = commonService.getListPolice();
+    window.setTimeout(function () {
+        map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 14,
+            center: commonService.myCoordinate,
+            mapTypeId: 'roadmap'
+        });
+        var marker = new google.maps.Marker({
+            position: commonService.myCoordinate,
+            map: map,
+            animation: google.maps.Animation.DROP,
+            title: 'This is NHÀ VIỆTTTTTTTTTTTTT',
+            icon: iconParty
+        });
+        for (var i = 0; i < listPolice.length; i++) {
+            var content = '<div id="iw-container">' +
+                    '<div class="iw-title">' + listPolice[i].note + '</div>' +                    
+                  '</div>';
+            var infowindow = new google.maps.InfoWindow({
+                content: content,
+                maxWidth: 350
+            });
+            var marker = new google.maps.Marker({
+                position: listPolice[i].coordinate,
+                map: map,
+                animation: google.maps.Animation.DROP,
+                title: listPolice[i].note,
+                icon: iconDanger
+            });
+            google.maps.event.addListener(marker, 'click', (function (marker, content, infowindow) {
+                return function () {
+                    infowindow.setContent(content);
+                    infowindow.open(map, marker);
+                };
+            })(marker, content, infowindow));
+            google.maps.event.addListener(map, 'click', function () {
+                infowindow.close();
+            });
+        }
+    }, 1000)
+});
---
+++ test/test/common.factory.js
@@ -0,0 +1,26 @@
+﻿angular.module('myApp')
+.factory('commonService', function () {
+    var listPolice = [
+        {
+            coordinate: {
+                lat: 21.120642,
+                lng: 105.826873
+            },
+            note:"Bắt đè vạch mắt võng chỗ rẽ lên cầu. Nếu bị bắt nộp 1 lít (đủ giấy tờ)"
+        },
+        {
+            coordinate: {
+                lat: 21.125094,
+                lng: 105.814971
+            },
+            note: "Công an khu vực, bắt rẽ không xi nhan, không mũ, nothing gương cũng bắt. Cẩn thận, team này hay núp lùm"
+        }
+    ]
+    function getListPolice() {
+        return listPolice;
+    }
+    return {
+        myCoordinate: { lat: 21.139425, lng: 105.813909 },
+        getListPolice: getListPolice
+    }
+})
